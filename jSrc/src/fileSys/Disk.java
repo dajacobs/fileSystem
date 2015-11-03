d34c02d7fb4b0137e0b1e3d556bafecaab9c6e29
dajacobs <dajacobs.tab@gmail.com>
2015-11-03 09:17:40 -0600

    disk construct

--- jSrc/src/fileSys/Disk.java
+++ jSrc/src/fileSys/Disk.java
@@ -1,14 +1,32 @@
 package fileSys;
 
 import java.io.File;
+import java.io.IOException;
 import java.io.RandomAccessFile;
 
 public class Disk {
+    // Size of bytes of each disk block
     public final static int BLOCK_SIZE = 512;
+    // Number of disk blocks in system
     public final static int NUM_BLOCKS = 1000;
+    // Number of pointers in a disk block
     public final static int POINTERS_PER_BLOCK = (BLOCK_SIZE/4);
+    // Number of reads to file system
     private final int readCount = 0;
+    // Number of writes to file system
     private final int writeCount = 0;
+    // File representing the disk
     private File fileName;
+    // RAF representing the disk
     private RandomAccessFile disk;
+    // Simulated disk
+    public Disk() {
+        try {
+            fileName = new File("DISK");
+            disk = new RandomAccessFile(fileName, "rw");
+        } catch(IOException e) {
+            System.err.println("Unable to start disk");
+            System.exit(1);
+        }
+    }
 }
