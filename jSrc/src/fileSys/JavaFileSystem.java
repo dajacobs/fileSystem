6c6412b59dfdd8910d444a69cdde74b36d658640
dajacobs <dajacobs.tab@gmail.com>
2015-11-08 16:16:27 -0600

    java file system initialization

---
+++ jSrc/src/fileSys/JavaFileSystem.java
@@ -0,0 +1,8 @@
+package fileSys;
+
+public class JavaFileSystem {
+    private Disk disk;
+    private SuperBlock superBlock;
+    private IndirectBlock freeList;
+    private FileTable fileTable;
+}
