915afe90c1ac5666209f642fbc39a9557a4b1d05
dajacobs <dajacobs.tab@gmail.com>
2015-11-08 16:24:00 -0600

    java file system constructor

--- jSrc/src/fileSys/JavaFileSystem.java
+++ jSrc/src/fileSys/JavaFileSystem.java
@@ -1,8 +1,26 @@
 package fileSys;
 
 public class JavaFileSystem {
-    private Disk disk;
-    private SuperBlock superBlock;
-    private IndirectBlock freeList;
-    private FileTable fileTable;
+    // Disk declaration
+    private final Disk disk;
+    // Super block declaration
+    private final SuperBlock superBlock;
+    // List cache
+    private final IndirectBlock freeList;
+    // File table declaration
+    private final FileTable fileTable;
+    
+    // Instantiate java file system
+    public JavaFileSystem() {
+        disk = new Disk();
+        superBlock = new SuperBlock();
+        fileTable = new FileTable();
+        freeList = null;
+        try {
+            disk.read(0, superBlock);
+        } catch (Exception e) {
+            System.err.println(e);
+            System.exit(1);
+        }
+    }
 }
