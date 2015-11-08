a82015f1788c55d8e67a6ab1bedd33a4cd3f266c
dajacobs <dajacobs.tab@gmail.com>
2015-11-08 16:32:34 -0600

    format disk func

--- jSrc/src/fileSys/JavaFileSystem.java
+++ jSrc/src/fileSys/JavaFileSystem.java
@@ -23,4 +23,36 @@ public class JavaFileSystem {
             System.exit(1);
         }
     }
+    // Format the disk image
+    public int formatDisk(int size, int iSize) {
+        InodeBlock ib = new InodeBlock();
+        // Mark as unused
+        for(Inode node : ib.node) {
+            node.flags = 0;
+        }
+        // Write the empty indoe block
+        for(int i = 1; i <= iSize; i++) {
+            disk.write(i, ib);
+        }
+        int offset = 127;
+        int lastFreeListBlock = 0;
+        
+        IndirectBlock b = new IndirectBlock();
+        for(int i = size - 1; i >= iSize + 1; i--) {
+            if((offset == 0) || (i == iSize + 1)) {
+                // Write and update the block reference
+                b.pointer[0] = lastFreeListBlock;
+                disk.write(i, b);
+                // Cache last free block
+                lastFreeListBlock = i;
+                // Prepare next block, if not last
+                if(i > iSize + 1) {
+                    offset = 127;
+                    b.clear();
+                } else {
+                    b.pointer[offset--] = i;
+                }
+            }
+        }
+    }
 }
