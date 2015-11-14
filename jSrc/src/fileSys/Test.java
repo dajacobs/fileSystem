9d6b2427f320ca5100d1473e58aabce061c4d6d2
dajacobs <dajacobs.tab@gmail.com>
2015-11-13 20:28:39 -0600

    help func

--- jSrc/src/fileSys/Test.java
+++ jSrc/src/fileSys/Test.java
@@ -11,5 +11,22 @@ public class Test {
         fs = new JavaFileSystem();
         // Buffered reader initialize
         BufferedReader data = new BufferedReader(new InputStreamReader(System.in));
+        
+    }
+    // Help list
+    private static void help() {
+        System.out.println("\tformat size iSize");
+        System.out.println("\tshutdown");
+        System.out.println("\tcreate");
+        System.out.println("\topen inum");
+        System.out.println("\tinumber fd");
+        System.out.println("\tread fd size");
+        System.out.println("\twrite fd pattern size");
+        System.out.println("\tseek fd offset whence");
+        System.out.println("\tclose fd");
+        System.out.println("\tdelete inum");
+        System.out.println("\tquit");
+        System.out.println("\tvars");
+        System.out.println("\thelp");
     }
 }
