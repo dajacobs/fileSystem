303276bd34ab8ac090e4124eeb2eaa07c92437e8
dajacobs <dajacobs.tab@gmail.com>
2015-11-27 21:29:54 -0600

    read/write integer func

---
+++ jSrc/src/fileSys/RWInt.java
@@ -0,0 +1,18 @@
+package fileSys;
+
+import java.io.RandomAccessFile;
+
+public class RWInt {
+    public static void main(String[] args) {
+        try {
+            try(RandomAccessFile raf = new RandomAccessFile("PATH_TO_FILE_YOU_WANT_CREATED", "rw")) {
+                long l = raf.length();
+                while(raf.getFilePointer() < 1) {
+                    System.out.println(raf.readByte());
+                }
+            }
+        } catch (Exception e) {
+            System.out.println("Error with reading/writing integer");
+        }
+    }
+}
